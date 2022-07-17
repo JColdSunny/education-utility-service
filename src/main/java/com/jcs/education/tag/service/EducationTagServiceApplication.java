111ff8e6a70c1c007c346054555c9ea628097fbd
Dmitri Bahaslouski <dbahaslouski@deem.com>
2022-07-17 14:23:49 +0300

    ETS - initial project structure

---
+++ src/main/java/com/jcs/education/tag/service/EducationTagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.education.tag.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationTagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationTagServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcs/education/tag/service/EducationTagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.education.tag.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationTagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
